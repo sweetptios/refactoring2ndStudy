88a6d2ba18f5169f543f90e04b2e88f91e82d3dd
cookie <cookie@shoplive.cloud>
2024-01-21 17:39:18 +0900

    적립포인트 계산 로직을 하위 클래스로 옮기기

--- JavaScript/createStatementData.js
+++ JavaScript/createStatementData.js
@@ -54,10 +54,14 @@ class ComedyCalulator extends PerformanceCalculator {
     get amount() {
         let result = 30000;
         if (this.performance.audience > 20) {
-            result += 10000 + 500 * (this.performance.audience - 20)
+            result += 10000 + 500 * (this.performance.audience - 20);
         }
         result += 300 * this.performance.audience;
         return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
 
@@ -72,10 +76,6 @@ class PerformanceCalculator {
     }
 
     get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-            if("comedy" =this.play.type) 
-                result += Math.floor(this.performance.audience / 5);
-        return result;
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
