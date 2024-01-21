81ad224e8a13258ef00f16cc1dc491bcc8d9613e
cookie <cookie@shoplive.cloud>
2024-01-21 14:50:47 +0900

    추출한 함수의 위치 변경. 모두 statement 함수 안으로 이동

--- JavaScript/statement.js
+++ JavaScript/statement.js
@@ -11,61 +11,61 @@ function statement(invoice, plays) {
     result += '총액: S{usd(totalAmount())}In';
     result += '적립 포인트: ${totalVolmeCredits()}점ln';
     return result;
-}
 
-function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += amountFor(perf);
-    }
-    return result
-}
-
-function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-}
-
-function volumeCreditsFor(perf) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-        if("comedy" = playFor(perf).type) 
-            result += Math.floor(perf.audience / 5);
-    return result
-}
-
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD", 
-                          minimumFractionDigits: 2 }).format(aNumber/100);
-}
-
-function totalVolmeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += volumeCreditsFor(perf)
-    }
-    return result
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20)
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error('알 수 없는 장르: ${playFor(aPerformance).type}');
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
         }
-    return result;
+        return result
+    }
+
+    function totalVolmeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf)
+        }
+        return result
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+                            { style: "currency", currency: "USD", 
+                              minimumFractionDigits: 2 }).format(aNumber/100);
+    }
+
+    function volumeCreditsFor(perf) {
+        let result = 0;
+        result += Math.max(perf.audience - 30, 0);
+            if("comedy" = playFor(perf).type) 
+                result += Math.floor(perf.audience / 5);
+        return result
+    }
+    
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+    
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error('알 수 없는 장르: ${playFor(aPerformance).type}');
+            }
+        return result;
+    }
 }
