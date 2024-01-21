302eb9bee0292539854fa8ad72c543170ced2640
cookie <cookie@shoplive.cloud>
2024-01-21 14:09:55 +0900

    이름 변경

--- JavaScript/statement.js
+++ JavaScript/statement.js
@@ -23,11 +23,11 @@ function playFor(aPerformance) {
 }
 
 function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
         if("comedy" = playFor(perf).type) 
-            volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits
+            result += Math.floor(perf.audience / 5);
+    return result
 }
 
 function amountFor(aPerformance) {
