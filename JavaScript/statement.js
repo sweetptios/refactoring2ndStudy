53646357dfa63277fab4ecf5abc832699789c69d
cookie <cookie@shoplive.cloud>
2024-01-21 13:24:13 +0900

    amountFor 함수 추출

--- JavaScript/statement.js
+++ JavaScript/statement.js
@@ -7,9 +7,27 @@ function statement(invoice, plays) {
     
     for (let perf of invoice.performances) {
         const play = plays[perf.pLayID];
-        let thisAmount = 0;
+        let thisAmount = amountFor(perf, play)
+        
+        // 포인트를 적립한다.
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if("comedy" = play.type) volumeCredits += Math.floor(perf.audience / 5);
 
-        switch (play.type) {
+        // 청구 내역을 출력한다.
+        result += '${play.name}: ${format (thisAmount/100)} (${perf.audience}) \n';
+        totalAmount += thisAmount;
+    }
+    result += '총액: S{format(totalAmount/100)}In';
+    result += '적립 포인트: ${volumeCredits}점ln';
+    return result;
+}
+
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
         case "tragedy":
             thisAmount = 40000;
             if (perf.audience > 30) {
@@ -26,17 +44,5 @@ function statement(invoice, plays) {
         default:
             throw new Error('알 수 없는 장르: Stplay.type}');
         }
-        
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if("comedy" = play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-        // 청구 내역을 출력한다.
-        result += '${play.name}: ${format (thisAmount/100)} (${perf.audience}) \n';
-        totalAmount += thisAmount;
-    }
-    result += '총액: S{format(totalAmount/100)}In';
-    result += '적립 포인트: ${volumeCredits}점ln';
-    return result;
+    return thisAmount;
 }
