bb0dfd73b844dbb7821a19b242b911dbe89b6d09
cookie <cookie@shoplive.cloud>
2024-01-21 17:18:57 +0900

    - 신규 파일이 stage에 올라가지 못해 커밋이 실패하고 있었음. 한꺼번에 커밋함.
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    	JavaScript/createStatementData.js
    
    1. 함수 선언 바꾸기
    2. amountFor 함수 옮기기
    3. 원본함수가 새로 만든 함수로 작업을 위임
    4. 새 함수 호출
    5. 적립 포인트 계산 로직을 계산기로 옯김

---
+++ JavaScript/createStatementData.js
@@ -0,0 +1,70 @@
+export default function createStatementData(invoice, plays) {
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
+
+    function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+  
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function totalAmount(data) {
+        return data.performances
+            .reduce((total, p) => total + p.amount, 0);
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances
+            .reduce((total, p) => total + p.amount, 0);
+    }
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance
+        this.play = aPlay
+    }
+
+    get amount() {
+        let result = 0;
+    
+        switch (this.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (this.play.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20)
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error('알 수 없는 장르: ${this.play.type}');
+            }
+        return result;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+            if("comedy" =this.play.type) 
+                result += Math.floor(this.performance.audience / 5);
+        return result;
+    }
+}
