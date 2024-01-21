9da08dc6fc597b8ff2bc0204e7a5891d3ffedf84
cookie <cookie@shoplive.cloud>
2024-01-21 16:25:23 +0900

    HTML 문서를 출력하는 함수 추가

--- JavaScript/statement.js
+++ JavaScript/statement.js
@@ -15,10 +15,28 @@ function renderPlainText(data, plays) {
     result += '총액: S{usd(data.totalAmount)}In';
     result += '적립 포인트: ${data.totalVolumeCredits}점ln';
     return result;
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-                            { style: "currency", currency: "USD", 
-                                minimumFractionDigits: 2 }).format(aNumber/100);
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml (data) {
+    let result='Ch1>청구 내역 (고객명: ${data. customer})</h12\n';
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data. performances) {
+        result += '  <tr><td>${perf.play.name</td><td>(${perf.audience}석)</td>;';
+        result += '<td>${usd(perf.amount)}</td></tr>\n';
     }
+    result += "</table>\n";
+    result += '<p>총액: <em>${usd(data.totalAmount)}</em></p>\n';
+    result += '<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n';
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+                        { style: "currency", currency: "USD", 
+                            minimumFractionDigits: 2 }).format(aNumber/100);
 }
