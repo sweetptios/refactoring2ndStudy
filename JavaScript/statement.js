91a58a76fe6e98495c0215a9cc9b76e7a0c28f05
cookie <cookie@shoplive.cloud>
2024-01-21 15:49:28 +0900

    volumeCredits 를 중간데이터로 변경

--- JavaScript/statement.js
+++ JavaScript/statement.js
@@ -8,7 +8,8 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
-        result.amount = amountFor(result)
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -38,6 +39,14 @@ function statement(invoice, plays) {
             }
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+            if("comedy" =aPerformance.play.type) 
+                result += Math.floor(aPerformance.audience / 5);
+        return result
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -63,7 +72,7 @@ function renderPlainText(data, plays) {
     function totalVolmeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf)
+            result += perf.volumeCredits
         }
         return result
     }
@@ -72,13 +81,5 @@ function renderPlainText(data, plays) {
         return new Intl.NumberFormat("en-US",
                             { style: "currency", currency: "USD", 
                                 minimumFractionDigits: 2 }).format(aNumber/100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-            if("comedy" =aPerformance.play.type) 
-                result += Math.floor(aPerformance.audience / 5);
-        return result
     }
 }
