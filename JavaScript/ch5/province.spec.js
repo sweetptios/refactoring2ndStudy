db1a29a06cdd0b4316053b0adac221df9f4dfbf8
sweetptios <sweetptios@gmail.com>
2024-02-24 21:24:52 +0900

    p144: 4.4 픽스처를 옮겨서 중복을 제거

--- JavaScript/ch5/province.spec.js
+++ JavaScript/ch5/province.spec.js
@@ -4,13 +4,12 @@ import { Province, sampleProvinceData } from './province.js';
 import Producer from './producer.js';
 
 describe('province', function() {
+    const asia = new Province(sampleProvinceData());
     it('shortfall', function() {
-        const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     });
 
     it('profit', function() {
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).equal(230);
     });
 });
